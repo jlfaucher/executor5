141f186e9a6b9dae29442f24bd6a4fb3b305b939
miesfeld <miesfeld@0b6cbdbe-3aab-466e-b73a-abd511dda0a2>
2012-02-26 23:03:15 +0000

    Back port fix for 3487781 DateTime class in Citrix environment raises error condition from trunk
    git-svn-id: svn+ssh://svn.code.sf.net/p/oorexx/code-0/main/branches/4.1@7593 0b6cbdbe-3aab-466e-b73a-abd511dda0a2

--- trunk/interpreter/platform/windows/TimeSupport.cpp
+++ trunk/interpreter/platform/windows/TimeSupport.cpp
@@ -93,6 +93,22 @@ void SystemInterpreter::getCurrentTime(RexxDateTime *Date )
 
     // ok, we can use this to calculate the timestamp directly
     Date->timeZoneOffset = localTimeStamp - sysTimeStamp;
+    // The Citrix environment gives a strange result here that is off by
+    // a few micro seconds.  This gives the DateTime class fits, so do an
+    // extra rounding operation here to the nearest second.  If the offset is
+    // negative, then do the rounding using the absolute value
+    if ( Date->timeZoneOffset < 0)
+    {
+        Date->timeZoneOffset = -Date->timeZoneOffset;
+        Date->timeZoneOffset = (Date->timeZoneOffset + 500000UL) / 1000000UL;
+        Date->timeZoneOffset *= 1000000UL;
+        Date->timeZoneOffset = -Date->timeZoneOffset;
+    }
+    else
+    {
+        Date->timeZoneOffset = (Date->timeZoneOffset + 500000UL) / 1000000UL;
+        Date->timeZoneOffset *= 1000000UL;
+    }
 
     SYSTEMTIME localTime;
 
